c72b8fdd43898e903aafd7e2ba272ab7265cd44f
Alan Barth <alancbarth@gmail.com>
2020-09-16 16:09:28 -0600

    Day 1

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Alan Barth on 9/16/20.
+//  Copyright © 2020 Alan Barth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+//    var students = ["Harry", "Hermoine", "Ron"]
+//
+//    @State private var selectedStudent = "Harry"
+    
+//    @State private var tapCount = 0
+    
+//    @State private var name = ""
+    
+    var body: some View {
+        Text("Hello World")
+        
+//        Picker("Select your student", selection: $selectedStudent) {
+//            ForEach(0 ..< students.count) {
+//                Text(self.students[$0])
+//            }
+//        }
+        
+//        Form {
+//            ForEach(0 ..< 100) {
+//                Text("Row \($0)")
+//            }
+//        }
+        
+//        Form {
+//            TextField("Enter your name", text: $name)
+//            Text("Your name is \(name)")
+//        }
+        
+//        NavigationView {
+//            Form {
+//                Section {
+//                    Text("Hello World")
+//                }
+//            }
+//        .navigationBarTitle("SwiftUI")
+//        }
+        
+//        Button("Tap Count \(tapCount)") {
+//            self.tapCount += 1
+//        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
