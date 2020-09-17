0359fe03e09810055dc1fbcacc86ee810c85704d
Alan Barth <alancbarth@gmail.com>
2020-09-16 21:36:44 -0600

    Day 17

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,46 +10,53 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    var students = ["Harry", "Hermoine", "Ron"]
-//
-//    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
     
-//    @State private var tapCount = 0
+    let tipPercentages = [10, 15, 20, 25, 0]
     
-//    @State private var name = ""
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 + tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     
     var body: some View {
-        Text("Hello World")
-        
-//        Picker("Select your student", selection: $selectedStudent) {
-//            ForEach(0 ..< students.count) {
-//                Text(self.students[$0])
-//            }
-//        }
-        
-//        Form {
-//            ForEach(0 ..< 100) {
-//                Text("Row \($0)")
-//            }
-//        }
-        
-//        Form {
-//            TextField("Enter your name", text: $name)
-//            Text("Your name is \(name)")
-//        }
-        
-//        NavigationView {
-//            Form {
-//                Section {
-//                    Text("Hello World")
-//                }
-//            }
-//        .navigationBarTitle("SwiftUI")
-//        }
-        
-//        Button("Tap Count \(tapCount)") {
-//            self.tapCount += 1
-//        }
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How much Tip do you want to leave?")) {
+                    
+                    Picker("Percent to Tip", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("We Split")
+        }
+
     }
 }
 
