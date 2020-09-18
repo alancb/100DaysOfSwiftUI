589db066d722082f8fe8e9f2531c074b997d52ac
Alan Barth <alancbarth@gmail.com>
2020-09-18 08:12:34 -0600

    Day 20

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alan Barth on 9/17/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
